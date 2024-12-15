067aec2da0fedfeb1fffca6e23defe91899834b5
jay-narayan <140262689+jay-narayan@users.noreply.github.com>
2024-12-15 17:54:53 +0530

    Update EmployeeController.java

--- src/main/java/com/EmployeeTest/controller/EmployeeController.java
+++ src/main/java/com/EmployeeTest/controller/EmployeeController.java
@@ -29,7 +29,7 @@ public class EmployeeController {
 
 		Employee saveEmployee = employeeRepo.save(emp);
 
-		return "Employee  successfully";
+		return "Employee  successfully"; // test
 	}
 
 	@GetMapping("/getAll")
